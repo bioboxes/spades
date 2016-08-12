3ca474fdaf702eaac7a744d3e999bcaef9bcb537
Michael Barton <mail@michaelbarton.me.uk>
2016-08-12 16:00:19 -0700

    Implement assembly script

--- image/bin/assemble.sh
+++ image/bin/assemble.sh
@@ -6,6 +6,19 @@ set -o xtrace
 
 TASK=$1
 CMD=$(fetch_task_from_taskfile.sh $TASKFILE $TASK)
-READS=$(biobox_args.sh 'select(has("fastq")) | .fastq | map(.value) | join(",")')
+TMP=$(mktemp -d)
+DATA=$(mktemp -d)/input.yaml
 
+yaml2json < ${INPUT} | jq --from-file /usr/local/share/spades_input.jq > ${DATA}
 eval ${CMD}
+
+cp ${TMP}/contigs.fasta ${OUTPUT}/contigs.fa
+
+cat << EOF > ${OUTPUT}/biobox.yaml
+version: 0.9.0
+arguments:
+  - fasta:
+    - id: contigs_1
+      value: contigs.fa
+      type: contigs
+EOF
