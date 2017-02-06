ac6dbf6b38e329cef97428a4add3b46960c5c243
Michael Barton <mail@michaelbarton.me.uk>
2017-02-06 11:54:31 -0800

    Update to spades/3.10.0

--- image/bin/install.sh
+++ image/bin/install.sh
@@ -7,15 +7,14 @@ set -o xtrace
 BUILD="wget ca-certificates"
 ESSENTIAL="python-minimal python-setuptools"
 
-URL="http://spades.bioinf.spbau.ru/release3.9.0/SPAdes-3.9.0-Linux.tar.gz"
+URL="http://spades.bioinf.spbau.ru/release${SPADES_VERSION}/SPAdes-${SPADES_VERSION}-Linux.tar.gz"
 
 # Build dependencies
 apt-get update --yes
 apt-get install --yes --no-install-recommends ${BUILD}
 
-# Install bbmap
-wget ${URL} --quiet --output-document - \
-  | tar xzf - --directory /usr/local/ --strip-components=1
+fetch_archive.sh ${URL} spades
+ln -s /usr/local/spades/bin/* /usr/local/bin/
 
 # Clean up dependencies
 apt-get autoremove --purge --yes ${BUILD}
